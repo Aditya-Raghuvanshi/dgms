dd2a1b3d37d8382e0acc480a7a7cf91c9570c435
Prince Raghuvanshi <adityaraghuvanshi159@gmail.com>
2024-04-12 01:36:37 +0530

    added audio in login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { insertName } from '../utils/adminSlice';
-// import Welcome from "../assets/Welcome.mp3"
+import Welcome from "../assets/Welcome.mp3"
 
 // import AudioPlayer from 'react-audio-player';
 //import useSound from 'use-sound';
@@ -13,8 +13,17 @@ const Login = () => {
     const [password,setPassword] = useState("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [audio, SetAudio] = useState("");
 
     const Authen=useSelector(store => store.admin); 
+
+    const Playit = async() => {
+      await audio.play();
+    };
+    useEffect(() => {
+      SetAudio(new Audio(Welcome));
+      // eslint-disable-next-line
+    }, []);
 
     const handleClick=()=>{
         // console.log(email,password);
@@ -38,7 +47,7 @@ const Login = () => {
       <img className='h-full w-full' src="https://img.freepik.com/free-vector/hand-painted-watercolor-abstract-watercolor-background_23-2149007169.jpg" alt="bg-img" /> 
       <form onSubmit={(e)=> e.preventDefault()} className='absolute bg-orange-500  w-3/12 mx-auto my-10 p-12 right-0 left-0 bg-opacity-85'>
             <h1 className='text-white text-3xl p-2 my-4 font-bold '>SIGN IN</h1>
-            <input onChange={(e)=> setName(e.target.value)} type="text" placeholder='Name' className='p-2 my-4 w-full bg-gray-600 text-white'/>
+            <input onChange={(e)=> setName(e.target.value)} onClick={()=>Playit()} type="text" placeholder='Name' className='p-2 my-4 w-full bg-gray-600 text-white'/>
             <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder='Email' className='p-2 my-4 w-full bg-gray-600 text-white'/><br/>
             <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Password' className='p-2 my-4 w-full bg-gray-600 text-white'/><br/>
             {/* <p className='text-md py-2 text-red-500'>{errMsg}</p> */}
